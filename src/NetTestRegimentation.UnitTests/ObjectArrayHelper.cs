c4e12f60f87be3f11d59e014fd1a97aa7c02be5d
David Vreony <dpvreony@users.noreply.github.com>
2025-06-09 21:01:27 +0100

    remove moq tests

--- src/NetTestRegimentation.UnitTests/ObjectArrayHelper.cs
+++ src/NetTestRegimentation.UnitTests/ObjectArrayHelper.cs
@@ -2,11 +2,8 @@
 // This file is licensed to you under the MIT license.
 // See the LICENSE file in the project root for full license information.
 
-using System;
 using System.Collections.Generic;
 using System.Globalization;
-using System.Text;
-using Moq;
 
 namespace NetTestRegimentation.UnitTests
 {
@@ -45,43 +42,6 @@ namespace NetTestRegimentation.UnitTests
             for (int i = 1; i <= size; i++)
             {
                 result.Add((i == argumentToLeaveNull) ? null : new FakeObject());
-            }
-
-            result.Add($"arg{(size > 1 ? argumentToLeaveNull.ToString(NumberFormatInfo.InvariantInfo) : string.Empty)}");
-
-            return result.ToArray();
-        }
-
-        /// <summary>
-        /// Gets an array of test objects for unit test member data sources utilising Moq.
-        /// </summary>
-        /// <param name="size">Size of the array.</param>
-        /// <returns>2 dimension array. 1st dimension is test scenario, 2nd is the arguments the method takes.</returns>
-        public static object[][] GetMoqArgObjectArrayOfArray(int size)
-        {
-            var result = new List<object[]>(size);
-            for (int i = 1; i <= size; i++)
-            {
-                result.Add(GetMoqArgObjectArray(size, i));
-            }
-
-            return result.ToArray();
-        }
-
-        /// <summary>
-        /// Gets an array of test moq objects for unit test member data sources.
-        /// </summary>
-        /// <param name="size">Size of the array.</param>
-        /// <param name="argumentToLeaveNull">1 based index of the argument to leave null.</param>
-        /// <returns>array. this is for the number of arguments the method takes.</returns>
-        public static object[] GetMoqArgObjectArray(
-            int size,
-            int argumentToLeaveNull)
-        {
-            var result = new List<object>(size + 1);
-            for (int i = 1; i <= size; i++)
-            {
-                result.Add((i == argumentToLeaveNull) ? null : new Mock<FakeObject>());
             }
 
             result.Add($"arg{(size > 1 ? argumentToLeaveNull.ToString(NumberFormatInfo.InvariantInfo) : string.Empty)}");
