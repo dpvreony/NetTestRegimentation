77e84b786ba36a438421e44745a73a6d8343fc2b
David Vreony <dpvreony@users.noreply.github.com>
2026-05-13 22:46:07 +0100

    add int test project

---
+++ src/NetTestRegimentation.IntegrationTests/FakeTest.cs
@@ -0,0 +1,25 @@
+﻿// Copyright (c) 2019 dpvreony and Contributors. All rights reserved.
+// This file is licensed to you under the MIT license.
+// See the LICENSE file in the project root for full license information.
+
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace NetTestRegimentation.IntegrationTests
+{
+    /// <summary>
+    /// Fake test to satisfy the requirement of having at least one test in the assembly for the test runner to execute.
+    /// </summary>
+    public static class FakeTest
+    {
+        /// <summary>
+        /// Test to ensure that the test runner can execute at least one test in the assembly. This test will always pass.
+        /// </summary>
+        public static void Run()
+        {
+            Assert.True(true);
+        }
+    }
+}
